43a198ccd1ddd4b45e22aefd244607691836bb33
Abdul Muiz <finaloot8@gmail.com>
2023-10-29 22:22:09 +0500

    stuff

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float life = 3;
+
+    //public variable to indicate shootables
+    public string shootableTag = "ENEMY";
+
+
     void Awake()
     {
+        // Destroy the bullet after a certain amount of time
         Destroy(gameObject, life);
     }
     void OnCollisionEnter(Collision collision)
     {
-        Destroy(collision.gameObject);
+        // Destroy the bullet and the object it collided with
+        //check if the object is an enemy
+        if (collision.gameObject.tag == "ENEMY")
+        {
+            //if it is, destroy it
+            Destroy(collision.gameObject);
+        }
         Destroy(gameObject);
     }
 }
